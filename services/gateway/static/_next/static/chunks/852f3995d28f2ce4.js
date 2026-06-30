573f805c5036a78b9ce859048fc5f6706793841e
Abdurrahman Balogun <63345153+mayowa-sys@users.noreply.github.com>
2026-06-30 20:21:18 +0100

    feat: serve Mintlify docs as static files from gateway at /docs (#98)

---
+++ services/gateway/static/_next/static/chunks/852f3995d28f2ce4.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/26a36330604bfb5b.js",
+  "static/chunks/9b9bafd346f2e26c.js",
+  "static/chunks/0768df1ae6a31175.js",
+  "static/chunks/turbopack-8258202e22b00089.js"
+])
